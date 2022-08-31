15b36e2f8698cb3d1cecc887c454cfc1f0b62bb7
Sarthak Tiwari <sarthaktiwari00001@gmail.com>
2022-08-31 19:17:28 +0530

    corrections

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,81 +5,68 @@ const Projects = (props) => {
   return (
     <div name='work' className='w-full md:h-screen text-gray-800 bg-blue-50'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
+        <div className='pb-2'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-800 border-pink-600'>
             Personal Projects
           </p>
           <p className='py-6'>Check out some of my recent work</p>
         </div>
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div
-            style={{ backgroundImage: `url("https://image.shutterstock.com/image-illustration/red-stamp-on-white-background-260nw-1165179109.jpg")` }}
-            className='shadow-lg  group container rounded-md flex justify-center items-center mx-auto content-div bg-gray-200'
-          >
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-center text-white tracking-wider'>
-                <h2>Hisab-Kitab</h2>
-                <p className="text-sm">
-                  Keep track of your expenses and balances and get a in depth analysis on your spendings.
-                </p>              </span>
-              <div className='pt-8 text-center'>
-                <a href="#_" className="inline-flex items-center justify-center mx-1 px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                  <span className="relative">Demo</span>
-                </a>
-                <a href="#_" className="inline-flex items-center justify-center mx-1 px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                  <span className="relative">Code</span>
-                </a>
-              </div>
+        <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-8'>
+          <div className="flex flex-col bg-slate-200 shadow-md rounded-lg p-2">
+            <span className="text-2xl tracking-widest font-mono font-bold my-2">HISAB KITAB</span>
+            <span className="text-xl mb-2">
+            Keep track of your expenses and balances and get an in depth analysis of your spendings.
+            </span>
+            <span className="text-xl font-mono font-medium">TECHNOLOGY STACK:</span>
+            <ul className="mx-8 list-disc">
+              <li>Golang</li>
+              <li>React</li>
+              <li>Mariadb</li>
+              <li>AWS</li>
+            </ul>
+            <div className="flex justify-center my-2">
+              <a href="https://hisabkitab.topnerd.space" target="_blank" rel="noreferrer" className="m-2 p-2 rounded active:scale-95 shadow-md outline outline-2 outline-blue-500 hover:bg-blue-500 hover:text-white">DEMO</a>
+              <a href="https://github.com/Sarthak001/hisabkitab" target="_blank" rel="noreferrer"  className="m-2 p-2 active:scale-95  rounded shadow-md outline outline-2 outline-blue-500 hover:bg-blue-500 hover:text-white">VIEW CODE</a> 
             </div>
           </div>
-          <div
-            style={{ backgroundImage: `url("https://image.shutterstock.com/image-illustration/red-stamp-on-white-background-260nw-1165179109.jpg")` }}
-            className='shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white text-center tracking-wider'>
-                <h2>ProShot</h2>
-                <p className="text-sm">
-                  A light and fast photo and video gallery. Access, manage and share photos to your loved ones from a single app.
-                </p>
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="#_" className="inline-flex items-center justify-center mx-1 px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                  <span className="relative">Demo</span>
-                </a>
-                <a href="#_" className="inline-flex items-center justify-center mx-1 px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                  <span className="relative">Code</span>
-                </a>
-              </div>
+          <div className="flex flex-col bg-slate-200 shadow-md rounded-lg p-2">
+            <span className="text-2xl tracking-widest font-mono font-bold my-2">PROSHOT</span>
+            <span className="text-xl mb-2">
+            A light and fast photo and video gallery. Access, manage and share photos with your loved ones.
+            </span>
+            <span className="text-xl font-mono font-medium">TECHNOLOGY STACK:</span>
+            <ul className="mx-8 list-disc">
+              <li>Python</li>
+              <li>React</li>
+              <li>Mariadb</li>
+              <li>AWS</li>
+            </ul>
+            <div className="flex justify-center my-2">
+              <a href="https://proshot.topnerd.space" target="_blank" rel="noreferrer" className="m-2 p-2 rounded active:scale-95 shadow-md outline outline-2 outline-blue-500 hover:bg-blue-500 hover:text-white">DEMO</a>
+              <a href="https://github.com/Sarthak001/proshot" target="_blank" rel="noreferrer"  className="m-2 p-2 rounded active:scale-95 shadow-md outline outline-2 outline-blue-500 hover:bg-blue-500 hover:text-white">VIEW CODE</a> 
             </div>
           </div>
-          <div
-            style={{ backgroundImage: `url("https://image.shutterstock.com/image-illustration/red-stamp-on-white-background-260nw-1165179109.jpg")` }}
-            className='shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white text-center tracking-wider'>
-                <h2>ProShot</h2>
-                <p className="text-sm">
-                  A light and fast photo and video gallery. Access, manage and share photos to your loved ones from a single app.
-                </p>
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="#_" className="inline-flex items-center justify-center mx-1 px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                  <span className="relative">Demo</span>
-                </a>
-                <a href="#_" className="inline-flex items-center justify-center mx-1 px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                  <span className="relative">Code</span>
-                </a>
-              </div>
-            </div>
+          <div className="flex flex-col bg-slate-200 shadow-md rounded-lg p-2">
+            <span className="text-2xl tracking-widest font-mono font-bold my-2">FILESHARING</span>
+            <span className="text-xl mb-2">
+            Filesharing server with basic authentication for file protection. Easy to setup and manage.
+            </span>
+            <span className="text-xl font-mono font-medium">TECHNOLOGY STACK:</span>
+            <ul className="mx-8 list-disc">
+              <li>Python</li>
+              <li>React</li>
+              <li>AWS</li>
+            </ul>
+            {/* <div className="flex justify-center">
+              <a className="m-2 p-2 rounded shadow-md outline outline-2 outline-blue-500 hover:bg-blue-500 hover:text-white">DEMO</a>
+              <a className="m-2 p-2 rounded shadow-md outline outline-2 outline-blue-500 hover:bg-blue-500 hover:text-white">VIEW CODE</a> 
+            </div> */}
           </div>
+
+          
+
+    
+
         </div>
       </div>
     </div>
